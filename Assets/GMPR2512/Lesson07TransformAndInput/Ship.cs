73dae0c9f883977b9a11ace21d82f13d4d441de8
Nolan Schuller <naschuller@shaw.ca>
2026-03-05 14:29:16 -0700

    end of day 16

--- Assets/GMPR2512/Lesson07TransformAndInput/Ship.cs
+++ Assets/GMPR2512/Lesson07TransformAndInput/Ship.cs
@@ -1,3 +1,4 @@
+using UnityEditor.SearchService;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -7,7 +8,8 @@ namespace GMPR2512.Lesson07TransformAndInput
     {
         [SerializeField] private float _movementSpeed = 5, _rotationSpeed = 200, _scaleSpeed = 5;
         [SerializeField] private float _minRotation = 25, _maxRotation = -25;
-        private InputAction _moveAction, _rotationAction, _scaleAction;
+        [SerializeField] private GameObject _projectilePrefab;
+        private InputAction _moveAction, _rotationAction, _scaleAction, _fireAction;
 
         void Awake()
         {
@@ -15,12 +17,43 @@ namespace GMPR2512.Lesson07TransformAndInput
             _moveAction = InputSystem.actions.FindAction("Player/Move");
             _rotationAction = InputSystem.actions.FindAction("Player/Move");
             _scaleAction = InputSystem.actions.FindAction("Player/Scale");
+            _fireAction = InputSystem.actions.FindAction("Player/Jump");
         }
+
+        // Unity will keep the input actions disabled by default
+        // for efficiency reasons. So we need to enable/disable them
+        // it is best practice to include the methods below
+        // the ? mark makes it so it does not run if null
+        void OnEnable()
+        {
+            _moveAction?.Enable();
+            _rotationAction?.Enable();
+            _scaleAction?.Enable();
+
+            if(_fireAction != null){
+            _fireAction.Enable();
+
+            // register methods with the fire action
+            _fireAction.performed += FireButtonPressed;
+            _fireAction.canceled += FireButtonReleased;
+            }
+        }
+
+        void OnDisable()
+        {
+            #region enable all
+            _moveAction?.Disable();
+            _rotationAction?.Disable();
+            _scaleAction?.Disable();
+            _fireAction?.Disable();
+            #endregion
+        }
+
         void Update()
         {
             Vector2 moveDirection = new Vector2(_moveAction.ReadValue<Vector2>().x, 0);
             Vector2 translation = moveDirection.normalized * _movementSpeed * Time.deltaTime;
-            transform.Translate(translation, Space.Self);
+            transform.Translate(translation, Space.World);
 
             float rotationValue = _rotationAction.ReadValue<Vector2>().normalized.y * _rotationSpeed * Time.deltaTime;
             transform.Rotate(0, 0, rotationValue);
@@ -57,6 +90,21 @@ namespace GMPR2512.Lesson07TransformAndInput
             }
 
             transform.localScale = scale;
+        }
+
+        private void FireButtonPressed(InputAction.CallbackContext context)
+        {
+            Vector3 projectileStartPosition = transform.GetChild(0).position;
+            GameObject theProjectile = Instantiate(_projectilePrefab, projectileStartPosition, transform.rotation);
+            Projectile projectileScript = theProjectile.GetComponent<Projectile>();
+            projectileScript.Speed = 5;
+            projectileScript.Direction = transform.up;
+
+        }
+
+        private void FireButtonReleased(InputAction.CallbackContext context)
+        {
+            
         }
     }
 }
